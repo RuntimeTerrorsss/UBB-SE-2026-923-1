98230fd822d800fa3ee9371f4ec11f3b21ea4974
Tea <teodoradiudea17@gmail.com>
2026-04-27 17:34:34 +0300

    added rt appxaml and part of ppg
    need to add the rest from powerpuff and check why vs wont see the models folder

--- BoardGamesApp/BookingBoardGames/App.xaml.cs
+++ BoardGamesApp/BookingBoardGames/App.xaml.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using BookingBoardGames.Src.Mapper;
+using BookingBoardGames.Src.Models;
+using BookingBoardGames.Src.Repositories;
+using BookingBoardGames.Src.Services;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -11,6 +15,7 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Microsoft.UI.Xaml.Shapes;
+using SearchAndBook.Services;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
@@ -29,10 +34,43 @@ namespace BookingBoardGames
         private Window? window;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="App"/> class.
-        /// Initializes the singleton application object.  This is the first line of authored code
+        /// Gets the initialization of the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
         /// </summary>
+        ///
+
+        public static InterfaceGeographicalService? GlobalGeoService { get; private set; }
+
+        public static UserRepository UserRepository { get; private set; } = new UserRepository();
+
+        public static IGameRepository GameRepository { get; private set; } = new GameRepository();
+
+        public static IRequestRepository RequestRepository { get; private set; } = new RequestRepository();
+
+        public static IRequestService RequestService { get; private set; } = new RequestService(RequestRepository, GameRepository);
+
+        public static PaymentRepository PaymentRepository { get; private set; } = new PaymentRepository();
+
+        public static ReceiptService ReceiptService { get; private set; } = new ReceiptService(UserRepository, RequestService, GameRepository);
+
+        public static CardPaymentService CardPaymentService { get; private set; } = new CardPaymentService(PaymentRepository,
+            UserRepository, ReceiptService, RequestService);
+
+        public static MapService MapService { get; private set; } = new MapService();
+
+        public static IRepositoryPayment HistoryRepository = new RepositoryPayment();
+
+        public static ServicePayment ServicePayment { get; private set; } = new ServicePayment(HistoryRepository,
+            ReceiptService);
+
+        public static CashPaymentService CashPaymentService { get; private set; } = new CashPaymentService(PaymentRepository,
+            new CashPaymentMapper(), ReceiptService);
+
+        public static ConversationRepository ConversationRepository { get; private set; } = new ConversationRepository();
+
+        public int DashboardUser = 3;
+        public int NoChatsUser = 8;
+
         public App()
         {
             this.InitializeComponent();
