7e48a373e633d1957616e2a339b2f90f357db68b
FloreaAndreiCiprian <andrei.ciprian.florea@stud.ubbcluj.ro>
2026-04-28 19:37:32 +0300

    deleted useless user files

--- BoardGamesApp/BookingBoardGames/Src/Repositories/InterfaceUsersRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BookingBoardGames.Src.Repositories;
-
-/// <summary>
-/// Defines the repository operations for managing User entities.
-/// </summary>
-public interface InterfaceUsersRepository : IRepository<User>
-{
-}
