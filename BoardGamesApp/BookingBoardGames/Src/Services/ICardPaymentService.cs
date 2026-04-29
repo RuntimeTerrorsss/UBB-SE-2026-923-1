b61c0018f6b5634a534e9e23965caa8b0eef8d82
György-Mózes Áron <aron.gyorgy@stud.ubbcluj.ro>
2026-04-29 16:19:37 +0300

    Added CardPaymentInterface

---
+++ BoardGamesApp/BookingBoardGames/Src/Services/ICardPaymentService.cs
@@ -0,0 +1,22 @@
+﻿using BookingBoardGames.Src.DTO;
+using BookingBoardGames.Src.Models;
+
+namespace BookingBoardGames.Src.Services
+{
+    public interface ICardPaymentService
+    {
+        CardPaymentDataTransferObject AddCardPayment(int requestIdentifier, int clientIdentifier, int ownerIdentifier, decimal amount);
+
+        bool CheckBalanceSufficiency(int requestIdentifier, int clientIdentifier);
+
+        CardPaymentDataTransferObject GetCardPayment(int paymentIdentifier);
+
+        decimal GetCurrentBalance(int clientIdentifier);
+
+        void ProcessPayment(int rentalIdentifier, int clientIdentifier, int ownerIdentifier);
+
+        CardPaymentDataTransferObject ConvertToDataTransferObject(Payment cardPayment);
+
+        RentalDataTransferObject GetRequestDataTransferObject(int rentalIdentifier);
+    }
+}
