146e0a4ead33ba54698cd5793317ee4882a59a6a
LarisaD52 <larisa.dragea@stud.ubbcluj.ro>
2026-05-01 12:51:03 +0300

    m2

--- BoardGamesApp/BookingBoardGames.Data/Models/City.cs
+++ BoardGamesApp/BookingBoardGames.Data/Models/City.cs
@@ -1,10 +1,23 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+[Table("cities")]
 public class City
 {
-    public int CityId { get; set; } 
+    [Key]
+    [Column("id")]
+    public int CityId { get; set; }
+
+    [Column("main_name")]
     required public string MainName { get; set; }
+
+    [Column("names")]
     required public List<string> Names { get; set; }
+
+    [Column("latitude")]
     public double Latitude { get; set; }
+
+    [Column("longitude")]
     public double Longitude { get; set; }
 }
