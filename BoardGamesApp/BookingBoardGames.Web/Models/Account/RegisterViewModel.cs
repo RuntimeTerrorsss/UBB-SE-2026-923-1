a31c129b2a8180789a1a1180ce4f67f223f4b45e
Carla <126351324+carla-draghiciu@users.noreply.github.com>
2026-05-14 21:00:31 +0300

    created register view model

---
+++ BoardGamesApp/BookingBoardGames.Web/Models/Account/RegisterViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookingBoardGames.Web.Models.Account
+{
+    public class RegisterViewModel
+    {
+        [Required] public string Username { get; set; }
+        [Required, EmailAddress] public string Email { get; set; }
+        [Required, MinLength(6)] public string Password { get; set; }
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
