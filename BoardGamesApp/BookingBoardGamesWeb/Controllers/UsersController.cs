a66d28ba5bfce378755333704bc712f1029b9768
Carla <126351324+carla-draghiciu@users.noreply.github.com>
2026-05-15 11:56:01 +0300

    yey register works

--- BoardGamesApp/BookingBoardGamesWeb/Controllers/UsersController.cs
+++ BoardGamesApp/BookingBoardGamesWeb/Controllers/UsersController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace BookingBoardGames.Api.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("[controller]")] // was api/
     public class UsersController : ControllerBase
     {
         private readonly IUserRepository _repo;
@@ -51,6 +51,17 @@ namespace BookingBoardGames.Api.Controllers
         {
             await _repo.UpdateBalance(id, newBalance);
             return NoContent();
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register([FromBody] User newUser)
+        {
+            var success = await _repo.Register(newUser);
+            if (!success)
+            {
+                return BadRequest("Registration failed. Username/Email already exists.");
+            }
+            return Ok();
         }
     }
 }
