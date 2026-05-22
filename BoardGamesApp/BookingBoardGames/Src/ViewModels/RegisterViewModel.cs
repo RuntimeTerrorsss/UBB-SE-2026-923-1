ca88d2f2505dab96b69962bdbc39407bf5f488b0
Carla <126351324+carla-draghiciu@users.noreply.github.com>
2026-05-22 12:28:52 +0300

    added commands

--- BoardGamesApp/BookingBoardGames/Src/ViewModels/RegisterViewModel.cs
+++ BoardGamesApp/BookingBoardGames/Src/ViewModels/RegisterViewModel.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using BookingBoardGames.Sharing.Services;
+using BookingBoardGames.Src.Commands;
 
 namespace BookingBoardGames.Src.ViewModels
 {
@@ -12,9 +14,24 @@ namespace BookingBoardGames.Src.ViewModels
     {
         private readonly IUserService userService;
 
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public event Action? NavigateToLogin;
+
+        public ICommand RegisterCommand { get; }
+
+        public ICommand GoToLoginCommand { get; }
+
         public RegisterViewModel(IUserService userService)
         {
             this.userService = userService;
+            RegisterCommand = new RelayCommand(async _ => await RegisterAsync());
+            GoToLoginCommand = new RelayCommand(_ => NavigateToLogin?.Invoke());
+        }
+
+        private async Task RegisterAsync()
+        {
+
         }
     }
 }
