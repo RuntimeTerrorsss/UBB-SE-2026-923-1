4c3dc318f734b6fe3bf9417fda9e3d54b00bddf8
Carla <126351324+carla-draghiciu@users.noreply.github.com>
2026-05-22 12:22:24 +0300

    created register view model

---
+++ BoardGamesApp/BookingBoardGames/Src/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingBoardGames.Src.ViewModels
+{
+    internal class RegisterViewModel
+    {
+    }
+}
