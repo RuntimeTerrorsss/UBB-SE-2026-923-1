6035cb65c8d8dd07b2e07349f4d5018aade6a990
Carla <126351324+carla-draghiciu@users.noreply.github.com>
2026-05-22 12:25:53 +0300

    created the constructor for register vm

--- BoardGamesApp/BookingBoardGames/Src/ViewModels/RegisterViewModel.cs
+++ BoardGamesApp/BookingBoardGames/Src/ViewModels/RegisterViewModel.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BookingBoardGames.Sharing.Services;
 
 namespace BookingBoardGames.Src.ViewModels
 {
-    internal class RegisterViewModel
+    public class RegisterViewModel : INotifyPropertyChanged
     {
+        private readonly IUserService userService;
+
+        public RegisterViewModel(IUserService userService)
+        {
+            this.userService = userService;
+        }
     }
 }
